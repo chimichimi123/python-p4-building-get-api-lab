e47fe848d3af59de46f646a5a6225bab5460ce6e
Christopher Cantu <chrischimi02@gmail.com>
2024-05-24 03:25:34 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, abort
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -20,19 +20,25 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries])
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = db.session.get(Bakery, id)
+    if bakery is None:
+        abort(404)
+    return jsonify(bakery.to_dict())
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([good.to_dict() for good in baked_goods])
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(baked_good.to_dict())
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
